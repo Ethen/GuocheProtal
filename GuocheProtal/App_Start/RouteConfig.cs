6e8ea2edb8f35c1bd126b1330df41678075a3c2d
Ethen <wuyingxiaosheng@sina.com>
2018-06-30 21:13:35 +0800

    first commit

---
+++ GuocheProtal/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GuocheProtal
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("contact", "contact", new { controller = "Home", action = "Contact" });
+            routes.MapRoute("about", "about", new { controller = "Home", action = "About" });
+            routes.MapRoute("service", "service", new { controller = "Home", action = "Service" });
+            routes.MapRoute("product", "product", new { controller = "Home", action = "Product" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ GuocheProtal/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuocheProtal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Index = 1;
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            ViewBag.Index = 3;
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            ViewBag.Index = 2;
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Index = 4;
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Index = 5;
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
